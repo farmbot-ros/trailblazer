8a654422e77604a0362e75b5a83bc2a14ee538f4
Trim Bresilla <trim.bresilla@gmail.com>
2024-12-11 03:24:03 +0100

    refactor: refactor launch setup to support `calculator` option
    - Update parameters in `params.yaml` to rename `ab_planner` to `gen_lines` and add a new `gen_path` section.
    - Introduce the `calculator` configuration option in `planner.launch.py`.
    - Replace the `ab_planner` node with a `gen_lines` node and conditionally add it to the nodes array if `calculator` is enabled.
    - Replace the `goto_field` node with a `gen_path` node and append it to the nodes array.
    - Conditionally include the `getthe_field` node based on the `calculator` flag.
    - Reintroduce the `goto_field` node as a separate step in the launch setup.

--- launch/planner.launch.py
+++ launch/planner.launch.py
@@ -14,25 +14,52 @@ def launch_setup(context, *args, **kwargs):
     namespace = LaunchConfiguration('namespace').perform(context)
     path_angle = LaunchConfiguration('path_angle').perform(context)
     alternate_freq = LaunchConfiguration('alternate_freq').perform(context)
+    calculator = LaunchConfiguration('calculator').perform(context)
     param_file = os.path.join(get_package_share_directory('farmbot_planner'), 'config', 'params.yaml')
 
     nodes_array = []
 
     print(alternate_freq)
 
-    ab_planner = Node(
+    is_calculator = False if calculator == '0' else True
+
+    gen_lines = Node(
         package='farmbot_planner',
-        executable="ab_planner",
-        name='ab_planner',
+        executable="gen_lines",
+        name='gen_lines',
         namespace=namespace,
         parameters=[
-            yaml.safe_load(open(param_file))['ab_planner']['ros__parameters'],
+            yaml.safe_load(open(param_file))['gen_lines']['ros__parameters'],
             yaml.safe_load(open(param_file))['global']['ros__parameters'],
             {'path_angle': path_angle} if path_angle != '' else {},
             {'alternate_freq': alternate_freq} if alternate_freq != '' else {}
         ]
     )
-    # nodes_array.append(ab_planner)
+    if is_calculator: nodes_array.append(gen_lines)
+
+    gen_path = Node(
+        package='farmbot_planner',
+        executable="gen_path",
+        name='gen_path',
+        namespace=namespace,
+        parameters=[
+            yaml.safe_load(open(param_file))['gen_path']['ros__parameters'],
+            yaml.safe_load(open(param_file))['global']['ros__parameters'],
+        ]
+    )
+    nodes_array.append(gen_path)
+
+    getthe_field = Node(
+        package='farmbot_planner',
+        executable="getthe_field",
+        name='getthe_field',
+        namespace=namespace,
+        parameters=[
+            yaml.safe_load(open(param_file))['getthe_field']['ros__parameters'],
+            yaml.safe_load(open(param_file))['global']['ros__parameters']
+        ]
+    )
+    if is_calculator: nodes_array.append(getthe_field)
 
     goto_field = Node(
         package='farmbot_planner',
@@ -45,18 +72,6 @@ def launch_setup(context, *args, **kwargs):
         ]
     )
     nodes_array.append(goto_field)
-
-    getthe_field = Node(
-        package='farmbot_planner',
-        executable="getthe_field",
-        name='getthe_field',
-        namespace=namespace,
-        parameters=[
-            yaml.safe_load(open(param_file))['getthe_field']['ros__parameters'],
-            yaml.safe_load(open(param_file))['global']['ros__parameters']
-        ]
-    )
-    nodes_array.append(getthe_field)
 
     to_nav = Node(
         package='farmbot_planner',
@@ -77,11 +92,13 @@ def generate_launch_description():
     namespace_arg = DeclareLaunchArgument('namespace', default_value='fbot')
     path_angle = DeclareLaunchArgument('path_angle', default_value='')
     alternate_freq = DeclareLaunchArgument('alternate_freq', default_value='')
+    calculator = DeclareLaunchArgument('calculator', default_value='0')
 
     return LaunchDescription([
             namespace_arg,
             path_angle,
             alternate_freq,
+            calculator,
             OpaqueFunction(function = launch_setup)
         ]
     )
