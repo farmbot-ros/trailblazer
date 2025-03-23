615e2735f5aca2a30425d34842a123b0447e70a4
Trim Bresilla <trim.bresilla@gmail.com>
2025-03-23 02:42:19 +0100

    feat: implement auction messaging and bid publishing capabilities
    - Add subscription to the auction topic with a callback for handling auction messages.
    - Implement a callback function to process auction messages, including logging them and iterating over their parameters.
    - Add publisher for sending bids to the bid topic.

--- src/bidder.cpp
+++ src/bidder.cpp
@@ -21,6 +21,21 @@ class Bidder {
             namespace_ = namespace_.substr(1);
         }
         RCLCPP_INFO(node->get_logger(), "Bidder [%s] started", namespace_.c_str());
+
+        auction_subscriber_ = node->create_subscription<farmbot_interfaces::msg::Auction>(
+            "/job/auction", 10, std::bind(&Bidder::auction_callback, this, std::placeholders::_1));
+        bid_publisher_ = node->create_publisher<farmbot_interfaces::msg::Bid>("/job/bid", 10);
+    }
+
+    void auction_callback(const farmbot_interfaces::msg::Auction::SharedPtr msg) {
+        if (msg->job_type != "harvest") {
+            return;
+        }
+        RCLCPP_INFO(node->get_logger(), "Auction with id [%s] received", msg->auction_id.c_str());
+        auto key_value = msg->parameters;
+        for (const auto &kv : key_value) {
+            RCLCPP_INFO(node->get_logger(), "%s: %s", kv.key.c_str(), kv.value.c_str());
+        }
     }
 
   private:
